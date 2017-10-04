ffc9e1e7ca1318dd006089a2e5cbf1b4453960f2
le <leduardo@lsd.ufcg.edu.br>
2017-10-04 16:05:43 -0300

    organization...

--- usr/lib/monasca/agent/custom_checks.d/lxc.py
+++ usr/lib/monasca/agent/custom_checks.d/lxc.py
@@ -35,9 +35,6 @@ class LXC(checks.AgentCheck):
         self.containers = self._containers_name()
         self.log.warning("Containers: {0}".format(str(self.containers)))
         for container_name in self.containers:
-            self.dimensions = self._set_dimensions({'container_name':
-                                                    container_name,
-                                                    'service': 'lxc'}, self.instance)
             self._collect_cpu_metrics(container_name)
             self._collect_mem_metrics(container_name)
             self._collect_net_metrics(container_name)
@@ -59,20 +56,20 @@ class LXC(checks.AgentCheck):
     def _collect_cpu_metrics(self, container_name):
         if not self.instance.get('cpu', True):
             return
-        self.log.warning('\tDimensions: '+ str(self.dimensions))
-        self.log.warning('\tInstance: '+ str(self.instance))
         metrics = self._get_cpu_metrics(container_name)
+        cpu_dimensions = self._get_dimensions(container_name)
         for metric, value in metrics.iteritems():
             self.log.warning('\tMetric: {0}, value: {1}'.format(metric,value))
-            self.gauge(metric, value, dimensions=self.dimensions)
+            self.gauge(metric, value, dimensions=cpu_dimensions)
 
     def _collect_mem_metrics(self, container_name):
         if not self.instance.get('mem', True):
             return
         metrics = self._get_mem_metrics(container_name)
+        mem_dimensions = self._get_dimensions(container_name)
         for metric, value in metrics.iteritems():
             self.log.warning('\tMetric: {0}, value: {1}'.format(metric,value))
-            self.gauge(metric, value, dimensions=self.dimensions)
+            self.gauge(metric, value, dimensions=mem_dimensions)
 
     def _collect_net_metrics(self, container_name):
         if not self.instance.get('net', True):
@@ -162,12 +159,13 @@ class LXC(checks.AgentCheck):
         return metrics
 
     def _get_dimensions(self, container_name, options={}):
-        dimensions={'container_name': container_name,
-                            'service': 'lxc'}
+        dimensions = {'container_name': container_name,
+                      'service': 'lxc'}
         dimensions.update(options)
         return self._set_dimensions(dimensions, self.instance)
 
     def _get_pid_container(self, container_name):
-        cpu_tasks = '{0}/{1}/tasks'.format(_LXC_CGROUP_CPU_PWD,container_name)
+        cpu_tasks = '{0}/{1}/tasks'.format(_LXC_CGROUP_CPU_PWD,
+                                           container_name)
         pid = open(cpu_tasks, 'r').readline().rstrip('\n')
         return pid
